b66f149248337dd4bee2391351e82bc4e029c906
zhangshukai1 <2438961693@qq.com>
2018-12-16 19:31:24 +0800

    这是一个student测试类

---
+++ src/main/java/com/tt/studentgit/StudentgitApplication.java
@@ -0,0 +1,14 @@
+package com.tt.studentgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentgitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentgitApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/tt/studentgit/StudentgitApplicationTests.java
@@ -0,0 +1,17 @@
+package com.tt.studentgit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StudentgitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
